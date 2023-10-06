635dca19fcd9cc8a9635df482182ff494f03cfc7
GRIGS <denislavpetrov12@gmail.com>
2023-10-06 15:19:34 +0300

    REST 1st request

--- test/WeAreSocial.Web/src/main/java/com/telerikacademy/infrastructure/selenium/api/RestApiCreateUser.java
+++ test/WeAreSocial.Web/src/main/java/com/telerikacademy/infrastructure/selenium/api/RestApiCreateUser.java
@@ -1,44 +1,75 @@
 package com.telerikacademy.infrastructure.selenium.api;
 
+import com.community.weare.Models.dto.UserDTO;
+import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import com.github.javafaker.Faker;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.List;
 
 public class RestApiCreateUser {
 
-    public static void main(String[] args) {
+    private String username;
+    private String password;
 
-        RestAssured.baseURI = "http://localhost:8081/api";
-
-        Faker faker = new Faker();
-        String professionId = String.valueOf(faker.number().numberBetween(101, 157));
-        String email = faker.internet().emailAddress();
-        String password = faker.internet().password();
-        String username = faker.name().username().replaceAll("[^a-zA-Z]", "");
-
-
-        String jsonPayload = "{\n" +
-                "  \"authorities\": [\"ROLE_USER\"],\n" +
-                "  \"category\": {\n" +
-                "    \"id\": " + professionId + ",\n" +
-                "    \"name\": \"CategoryName\"\n" +
-                "  },\n" +
-                "  \"confirmPassword\": \"" + password + "\",\n" +
-                "  \"email\": \"" + email + "\",\n" +
-                "  \"password\": \"" + password + "\",\n" +
-                "  \"username\": \"" + username + "\"\n" +
-                "}";
-
-
-        Response response = RestAssured.given()
+    private RequestSpecification getRestAssured() {
+        String baseUrl = "http://localhost:8081";
+        return RestAssured
+                .given()
                 .contentType(ContentType.JSON)
-                .body(jsonPayload)
-                .post("/users/");
+                .log().all()
+                .baseUri(baseUrl);
+    }
 
+    public void createUser() {
+        UserDTO newUser = generateRandomUser();
 
-        System.out.println("Status Code: " + response.getStatusCode());
-        System.out.println("Response Body: " + response.getBody().asString());
+        Response createUserResponse = createNewUser(newUser);
+        System.out.println("Create User Status Code: " + createUserResponse.getStatusCode());
+        System.out.println("Create User Response Body: " + createUserResponse.getBody().asString());
+    }
+
+    public void authenticateUser() {
+        Response authenticateUserResponse = authenticateUser(username, password);
+        System.out.println("Authenticate User Status Code: " + authenticateUserResponse.getStatusCode());
+    }
+
+    public UserDTO generateRandomUser() {
+        Faker faker = new Faker();
+
+        username = faker.name().username().replaceAll("[^a-zA-Z]", "");
+        password = faker.internet().password();
+        String email = faker.internet().emailAddress();
+
+        List<String> authorities = List.of("ROLE_USER");
+
+        UserDTO user = new UserDTO(username, password, authorities);
+        user.setEmail(email);
+        user.setConfirmPassword(password);
+
+        return user;
+    }
+
+    public Response createNewUser(UserDTO user) {
+        return getRestAssured()
+                .body(user)
+                .when()
+                .post("/api/users/")
+                .then()
+                .extract()
+                .response();
+    }
+
+    public Response authenticateUser(String username, String password) {
+        return getRestAssured()
+                .queryParam("username", username)
+                .queryParam("password", password)
+                .when()
+                .post("/authenticate")
+                .then()
+                .extract()
+                .response();
     }
 }
-
---
+++ test/WeAreSocial.Web/src/test/java/test/WEareSocial/RESTAssuredTests.java
@@ -0,0 +1,13 @@
+package test.WEareSocial;
+
+import com.telerikacademy.infrastructure.selenium.api.RestApiCreateUser;
+import org.junit.jupiter.api.Test;
+
+public class RESTAssuredTests {
+    private final RestApiCreateUser apiUser = new RestApiCreateUser();
+    @Test
+    public void testCreateUser() {
+        apiUser.createUser();
+
+    }
+}
